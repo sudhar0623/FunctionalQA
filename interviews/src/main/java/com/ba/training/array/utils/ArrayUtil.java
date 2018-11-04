1b06faad882a0e5a7878b875e92049e4ec16280d
Sudharani Sudharani <srani3@apple.com>
2018-11-04 10:32:04 -0800

    http://radar-0006: added removeDuplicates method.

--- interviews/src/main/java/com/ba/training/array/utils/ArrayUtil.java
+++ interviews/src/main/java/com/ba/training/array/utils/ArrayUtil.java
@@ -1,5 +1,8 @@
 package com.ba.training.array.utils;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ArrayUtil {
 
 	public static int[] reverseArray(int[] array) {
@@ -18,4 +21,22 @@ public class ArrayUtil {
 		return reverseArray;
 	}
 
+	public static Integer[] removeDuplicates(int[] arr) {
+		{
+			Set<Integer> set = new HashSet<Integer>();
+			for (int i = 0; i < arr.length; i++) {
+				set.add(arr[i]);
+			}
+			Integer[] newArr = new Integer[set.size()];
+			return set.toArray(newArr);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 3, 4, 5, 2, 1 };
+		Integer[] newArray = removeDuplicates(arr);
+		for (int i = 0; i <= newArray.length - 1; i++) {
+			System.out.print(newArray[i] + " ");
+		}
+	}
 }
